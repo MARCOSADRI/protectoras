4b085fdc355f2e71d87004cdf750420fa8db3660
MarcosTrasan <72410546+MarcosTrasan@users.noreply.github.com>
2021-04-24 19:05:53 +0200

    24/04

--- src/Form/AnimalesType.php
+++ src/Form/AnimalesType.php
@@ -3,7 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Animales;
+use Doctrine\ORM\EntityRepository;
+use Egulias\EmailValidator\Result\Reason\LabelTooLong;
+use PhpParser\Node\Stmt\Label;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +21,29 @@ class AnimalesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('especie')
+            ->add('especie', ChoiceType::class,  array(               
+                'choices'  => array(
+                    'Perro' => 'Perro',
+                    'Gato' => 'Gato',
+                    'Roedores/Varios' => 'Otros',
+                )))
             ->add('raza')
-            ->add('tamano')
-            ->add('sexo')
+            ->add('tamano', ChoiceType::class,  array(
+                'choices'  => array(
+                    'Pequeño' => 'Pequeño',
+                    'Mediano' => 'Mediano',
+                    'Grande' => 'Grande',
+                )))
+            ->add('sexo', ChoiceType::class,  array(  
+                'choices'  => array(
+                    'Macho' => 'Macho',
+                    'Hembra' => 'Hembra',
+                )))
             ->add('edad')
-            ->add('nombreA')
+            ->add('nombreA',TextType::class,['label' => 'Nombre: '])
             /* ->add('Adoptador') */
             ->add('ficha')/* AUTOMÁTICO */
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
